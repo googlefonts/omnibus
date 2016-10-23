79c4fcbafe2292ec38c951aeb7b2213908d23a66
Amin Al Hazwani <amin.alhazwani@gmail.com>
2016-10-23 18:11:11 +0200

    [edit] function class

--- src/js/randomCase.js
+++ src/js/randomCase.js
@@ -4,7 +4,7 @@ const styleClasses = ['u-fs-italic', 'u-fs-normal'];
 const caseClasses = ['u-tt-uppercase', 'u-tt-lowercase'];
 
 function setRandomClass() {
-  $('.js-faustina__header span').each(function applyClass() {
+  $('.js-faustina-title span').each(function applyClass() {
     $(this).removeClass();
     $(this).addClass(styleClasses[~~(Math.random() * styleClasses.length)]);
     $(this).addClass(caseClasses[~~(Math.random() * caseClasses.length)]);
