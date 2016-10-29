dd85d0d47958148365e973c974f2fdd204d77a1c
Amin Al Hazwani <amin.alhazwani@gmail.com>
2016-10-29 13:13:42 +0200

    [style] footer faustina

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 import Hypher from '../../custom_modules/vanilla-hypher.js';
-
 const h = new hyphenate('p.u-hyphenate', 'en-us');
+
+const main = document.getElementsByClassName('l-main');
+console.log(main);
+
+function disableMainScroll() {
+  main.style.overflowX = 'hidden';
+}
+
+function enableMainScroll() {
+  main.style.overflowX = 'auto';
+}
