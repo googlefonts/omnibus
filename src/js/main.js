0217a1ef3ed4c91c8dc7ed1dc0b6165b72a229b8
Amin Al Hazwani <amin.alhazwani@gmail.com>
2016-10-29 10:49:42 +0200

    [delete] random case script

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-import $ from 'jquery';
 import Hypher from '../../custom_modules/vanilla-hypher.js';
-import setRandomClass from '../js/randomCase.js';
 
 const h = new hyphenate('p.u-hyphenate', 'en-us');
-
-setRandomClass();
