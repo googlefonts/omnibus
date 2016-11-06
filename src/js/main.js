33fdf7108d6864ce7200e4007fd491429ee0a6c3
Amin Al Hazwani <amin.alhazwani@gmail.com>
2016-11-06 16:59:32 +0100

    [minor] main.js edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
 import Hypher from '../../custom_modules/vanilla-hypher.js';
 const h = new hyphenate('p.u-hyphenate', 'en-us');
 
-const main = document.getElementsByClassName('l-main');
-console.log(main);
-
 function disableMainScroll() {
   main.style.overflowX = 'hidden';
 }
