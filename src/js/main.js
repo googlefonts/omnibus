ad6790872f1899eae31a197ff44044a1a977f6eb
Amin Al Hazwani <amin.alhazwani@gmail.com>
2016-11-27 15:56:34 +0100

    [script] snap scroll polyfill

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import gfBadge from '../../custom_modules/googlefontsbadge.js';
 import Hypher from '../../custom_modules/vanilla-hypher.js';
+import scrollSnap from '../../custom_modules/scrollsnap-polyfill.js';
 
 const h = new hyphenate('p.u-hyphenate', 'en-us');
 gfBadge();
-
