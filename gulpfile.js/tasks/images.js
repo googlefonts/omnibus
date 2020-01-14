9c83d408a4668cbfc775354c64f9c4289cd28d9f
kennethormandy <hello@kennethormandy.com>
2020-01-13 21:06:27 -0800

    Temprarily removes minimally used imageoptim from Gulpfile

--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -9,7 +9,8 @@ const browserSync = require('browser-sync');
 gulp.task('images', function() {
   return gulp.src(config.src)
     .pipe(changed(config.dest)) // Ignore unchanged files
-    .pipe(imagemin()) // Optimize
+    // Temporarily removed as it’s breaking the build
+    // .pipe(imagemin()) // Optimize
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({ stream: true }));
 });
